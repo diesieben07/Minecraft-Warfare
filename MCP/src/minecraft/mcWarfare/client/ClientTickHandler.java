fc10db6f2be264a5e91b2815907f76077cf80575
diesieben07 <diesieben07@gmail.com>
2012-10-27 14:04:38 +0200

    Simplified the check if our button has already been added to the menu

--- MCP/src/minecraft/mcWarfare/client/ClientTickHandler.java
+++ MCP/src/minecraft/mcWarfare/client/ClientTickHandler.java
@@ -18,9 +18,7 @@ import static mcWarfare.common.MinecraftWarfare.*;
 public class ClientTickHandler implements ITickHandler {
 	
 	private final Minecraft mc;
-	private GuiScreen theMainMenu;
-	private int prevWidth;
-	private int prevHeight;
+	private GuiButton theButton;
 	
 	public ClientTickHandler(Minecraft mc) {
 		this.mc = mc;
@@ -31,25 +29,20 @@ public class ClientTickHandler implements ITickHandler {
 
 	@Override
 	public void tickEnd(EnumSet<TickType> type, Object... tickData) {
-		if (mc.currentScreen != null && mc.currentScreen instanceof GuiMainMenu && (theMainMenu != mc.currentScreen || prevWidth != mc.currentScreen.width || prevHeight != mc.currentScreen.height)) {
-			try {
-				// store the info, so we don't add the button twice
-				theMainMenu = mc.currentScreen;
-				prevWidth = theMainMenu.width;
-				prevHeight = theMainMenu.height;
-				
-				// create the button
-				GuiButton theButton = new ButtonMainMenu(1, theMainMenu.width / 2 + 2, theMainMenu.height / 4 + 48, 98, 20, "Minecraft Warfare");
-				// get the controlList from the screen
-				List<GuiButton> controlList = ObfuscationReflectionHelper.<List<GuiButton>,GuiScreen>getPrivateValue(GuiScreen.class, theMainMenu, 3);
-				// add our button so it will be drawn
+		if (mc.currentScreen != null && mc.currentScreen instanceof GuiMainMenu) {
+			List<GuiButton> controlList = ObfuscationReflectionHelper.<List<GuiButton>,GuiScreen>getPrivateValue(GuiScreen.class, mc.currentScreen, 3);
+			
+			if (theButton == null || !controlList.contains(theButton)) {
+				System.out.println("adding");
+				// (re-)create our button
+				theButton = new ButtonMainMenu(1, mc.currentScreen.width / 2 + 2, mc.currentScreen.height / 4 + 48, 98, 20, "Minecraft Warfare");
+				// and add it to the list
 				controlList.add(theButton);
+				
 				// get the "Singleplayer" button from the controlList
 				GuiButton singlePlayerButton = controlList.get(0);
 				// reduce the width of the button
 				ObfuscationReflectionHelper.<GuiButton,Integer>setPrivateValue(GuiButton.class, singlePlayerButton, Integer.valueOf(98), 0);
-			} catch (Exception e) {
-				logger.warning("Unable to access GuiMainMenu and add Button!");	
 			}
 		}
 	}
