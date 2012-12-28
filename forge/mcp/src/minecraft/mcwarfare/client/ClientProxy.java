7d50d08d64d3b9f573c6ca15ba4e7d9f84873527
diesieben07 <diesieben07@gmail.com>
2012-12-28 17:53:07 +0100

    Basic mod structure

---
+++ forge/mcp/src/minecraft/mcwarfare/client/ClientProxy.java
@@ -0,0 +1,7 @@
+package mcwarfare.client;
+
+import mcwarfare.common.CommonProxy;
+
+public class ClientProxy extends CommonProxy {
+
+}
--- forge/mcp/src/minecraft/mcwarfare/common/MinecraftWarfare.java
+++ forge/mcp/src/minecraft/mcwarfare/common/MinecraftWarfare.java
@@ -1,9 +1,29 @@
 package mcwarfare.common;
 
+import java.util.logging.Logger;
+
+import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid = "mcWarfare", name = "Minecraft Warfare", version = "0.1")
+@NetworkMod(versionBounds = "0.1")
 public class MinecraftWarfare {
 	
-	//hallo 
-	// hallo 2
-	//hallo 3
-
+	@SidedProxy(clientSide = "mcwarfare.client.ClientProxy", serverSide = "mcwarfare.common.CommonProxy")
+	public static CommonProxy proxy;
+	
+	public static Logger logger;
+	
+	@Init
+	public void init(FMLInitializationEvent evt) {
+		logger = Logger.getLogger("Minecraft Warfare");
+		logger.setParent(FMLLog.getLogger());
+		logger.setUseParentHandlers(true);
+		
+		logger.info("Initializing...");
+	}
 }
