095a0c4573fdd1129fbfa17bd868e3d8b8f747e9
diesieben07 <diesieben07@gmail.com>
2012-12-29 16:21:58 +0100

    Updated bullet

--- forge/mcp/src/minecraft/mcwarfare/common/EntityBullet.java
+++ forge/mcp/src/minecraft/mcwarfare/common/EntityBullet.java
@@ -5,17 +5,24 @@ import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.IProjectile;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.entity.projectile.EntityThrowable;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.MathHelper;
+import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 
-public class EntityBullet extends EntityArrow {
+public class EntityBullet extends EntityThrowable {
 
 	public EntityBullet(World world) {
 		super(world);
 	}
 	
-	public EntityBullet(World par1World, EntityLiving par2EntityLiving, float par3) {
-        super(par1World, par2EntityLiving, par3);
+	public EntityBullet(World world, EntityLiving living) {
+        super(world, living);
     }
+
+	@Override
+	protected void onImpact(MovingObjectPosition impact) {
+		
+	}
 }
--- forge/mcp/src/minecraft/mcwarfare/common/items/ItemGun.java
+++ forge/mcp/src/minecraft/mcwarfare/common/items/ItemGun.java
@@ -1,9 +1,7 @@
 package mcwarfare.common.items;
 
-import cpw.mods.fml.common.registry.LanguageRegistry;
 import mcwarfare.common.EntityBullet;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
 import net.minecraft.world.World;
 
 public class ItemGun extends ItemWarfare {
@@ -15,7 +13,7 @@ public class ItemGun extends ItemWarfare {
 	@Override
 	public void onItemLeftClick(EntityPlayer player, World world) {
 		if (!world.isRemote) {
-			EntityBullet bullet = new EntityBullet(world, player, 2);
+			EntityBullet bullet = new EntityBullet(world, player);
 			
 			
 			bullet.setPosition(player.posX, player.posY + player.getEyeHeight(), player.posZ);
