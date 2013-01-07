f34680de34d2ff6f29abb4babc9f5c53e09ca3ce
Alexander <cr3wp@yahoo.de>
2013-01-07 23:44:15 +0100

    kp

--- forge/mcp/src/minecraft/mcwarfare/common/items/GunType.java
+++ forge/mcp/src/minecraft/mcwarfare/common/items/GunType.java
@@ -4,58 +4,58 @@ import net.minecraft.item.ItemStack;
 
 public enum GunType {
 	
-	NORMAL("normal", 0, 10, 0, 0, 0, 0),
+	NORMAL("normal", 0, 10, 0, 0, 0, 0, 0, 0, 0),
 	//AssaultRifles
-	AK47("AK47", 0, 10, 0, 0, 0, 10),
-	ARX160("ARX160", 0, 10, 0, 0, 0, 0),
-	ACR("ACR", 0, 10, 0, 0, 0, 0),
-	CM901("CM901", 0, 10, 0, 0, 0, 0),
-	TAR21("TAR21", 0, 10, 0, 0, 0, 0),
-	FAMAS("FAMAS", 0, 10, 0, 0, 0, 0),
-	F2000("F2000", 0, 10, 0, 0, 0, 0),
-	G36("G36", 0, 10, 0, 0, 0, 0),
-	M16A3("M16A3", 0, 10, 0, 0, 0, 0),
-	M16A4("M16A4", 0, 10, 0, 0, 0, 0),
-	SCARl("SCARL", 0, 10, 0, 0, 0, 0),
-	AUG("AUG", 0, 10, 0, 0, 0, 0),
-	AR15("AR15", 0, 10, 0, 0, 0, 0),
+	AK47("AK47", 0, 10, 0, 0, 0, 10, 0, 0, 1),
+	ARX160("ARX160", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	ACR("ACR", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	CM901("CM901", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	TAR21("TAR21", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	FAMAS("FAMAS", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	F2000("F2000", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	G36("G36", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	M16A3("M16A3", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	M16A4("M16A4", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	SCARl("SCARL", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	AUG("AUG", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	AR15("AR15", 0, 10, 0, 0, 0, 0, 0, 0, 0),
 	//ShotGuns
-	AA12("AA12", 0, 10, 0, 0, 0, 0),
-	KSG("KSG", 0, 10, 0, 0, 0, 0),
-	USAS("USAS", 0, 10, 0, 0, 0, 0),
-	SPAS12("SPAS12", 0, 10, 0, 0, 0, 0),
+	AA12("AA12", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	KSG("KSG", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	USAS("USAS", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	SPAS12("SPAS12", 0, 10, 0, 0, 0, 0, 0, 0, 0),
 	//Snipers
-	SKS("SKS", 0, 10, 0, 0, 0, 0),
-	SVD("SVD", 0, 10, 0, 0, 0, 0),
-	L118A("L118A", 0, 10, 0, 0, 0, 0),
-	M95("M95", 0, 10, 0, 0, 0, 0),
-	AS50("AS50", 0, 10, 0, 0, 0, 0),
+	SKS("SKS", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	SVD("SVD", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	L118A("L118A", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	M95("M95", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	AS50("AS50", 0, 10, 0, 0, 0, 0, 0, 0, 0),
 	//Carabines
-	M4A1("M4A1", 0, 10, 0, 0, 0, 0),
-    SCARH("SCARH", 0, 10, 0, 0, 0, 0),
-    G36C("G36C", 0, 10, 0, 0, 0, 0),
-    SIG552("SIG552", 0, 10, 0, 0, 0, 0),
-    A91("A91", 0, 10, 0, 0, 0, 0),
+	M4A1("M4A1", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+    SCARH("SCARH", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+    G36C("G36C", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+    SIG552("SIG552", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+    A91("A91", 0, 10, 0, 0, 0, 0, 0, 0, 0),
 	//Pistols
-    MAGNUM("MAGNUM", 0, 10, 0, 0, 0, 0),
-    DESERTEAGLE("DESERTEAGLE", 0, 10, 0, 0, 0, 0),
-    G18("G18", 0, 10, 0, 0, 0, 0),
-    G17("G17", 0, 10, 0, 0, 0, 0),
-    MP412("MP412", 0, 10, 0, 0, 0, 0),
-    USP("USP", 0, 10, 0, 0, 0, 0),
+    MAGNUM("MAGNUM", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+    DESERTEAGLE("DESERTEAGLE", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+    G18("G18", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+    G17("G17", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+    MP412("MP412", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+    USP("USP", 0, 10, 0, 0, 0, 0, 0, 0, 0),
 	//SMG
-	AK74("AK74", 0, 10, 0, 0, 0, 0),
-	MP5("MP5", 0, 10, 0, 0, 0, 0),
-	MP7("MP7", 0, 10, 0, 0, 0, 0),
-	MP9("MP9", 0, 10, 0, 0, 0, 0),
-	P90("P90", 0, 10, 0, 0, 0, 0),
-	UMP45("UMP45", 0, 10, 0, 0, 0, 0),
+	AK74("AK74", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	MP5("MP5", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	MP7("MP7", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	MP9("MP9", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	P90("P90", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	UMP45("UMP45", 0, 10, 0, 0, 0, 0, 0, 0, 0),
 	//LMG
-	MG36("MG36", 0, 10, 0, 0, 0, 0),
-	M60("M60", 0, 10, 0, 0, 0, 0),
-	RPK("RPK", 0, 10, 0, 0, 0, 0),
-	M249("M249", 0, 10, 0, 0, 0, 0),
-	PKP("PKP", 0, 10, 0, 0, 0, 0);
+	MG36("MG36", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	M60("M60", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	RPK("RPK", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	M249("M249", 0, 10, 0, 0, 0, 0, 0, 0, 0),
+	PKP("PKP", 0, 10, 0, 0, 0, 0, 0, 0, 0);
 	
 	private final int bulletDamage;
 	private final String name;
@@ -65,14 +65,17 @@ public enum GunType {
 	
 	private final int fmode; //0=SEMI 1=AUTO 2=BURST
 	private final int bspeed;
+	private final int magsize;
+	private final int cooldown;
+	private final int boltaction; //0=NO 1=YES
 	
 	private int shootSpeed;
 	
-	private GunType(String name, int textureIndex, int bulletDamage, int vrecoil, int hrecoil, int fmode, int bspeed) {
-		this(name, textureIndex, bulletDamage, vrecoil, hrecoil, fmode, bspeed, -1);
+	private GunType(String name, int textureIndex, int bulletDamage, int vrecoil, int hrecoil, int fmode, int bspeed, int magsize, int cooldown, int boltaction) {
+		this(name, textureIndex, bulletDamage, vrecoil, hrecoil, fmode, bspeed, magsize, cooldown, boltaction, -1);
 	}
 	
-	private GunType(String name, int textureIndex, int bulletDamage, int vrecoil, int hrecoil, int fmode, int bspeed, int shootSpeed) {
+	private GunType(String name, int textureIndex, int bulletDamage, int vrecoil, int hrecoil, int fmode, int bspeed, int magsize, int cooldown, int boltaction, int shootSpeed) {
 		this.bulletDamage = bulletDamage;
 		this.name = name;
 		this.textureIndex = textureIndex;
@@ -81,6 +84,10 @@ public enum GunType {
 		this.fmode = fmode;
 		this.bspeed = bspeed;
 		this.shootSpeed = shootSpeed;
+		this.magsize = magsize;
+		this.cooldown = cooldown;
+		this.boltaction = boltaction;
+		
 	}
 	
 	public static GunType byId(int id) {
@@ -120,7 +127,15 @@ public enum GunType {
 	public int getBspeed() {
 		return bspeed;
 	}
-	
+	public int getMagsize() {
+		return magsize;
+	}
+	public int getCooldown() {
+		return cooldown;
+	}
+	public int getBoltaction() {
+		return boltaction;
+	}
 	public boolean shouldShootAtTick(int tickCount) {
 		return shootSpeed == -1 ? tickCount == 0 : tickCount % shootSpeed == 0;
 	}
